75b2ac52e9519a1524681f545b542716efe38901
saravanaguhan19 <saravanaguhan19@gmail.com>
2024-08-29 21:51:38 +0530

    basic socket connection done

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use("/", express.static(__dirname + "/public"));
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+});
+
+server.listen(3000, () => {
+  console.log("Server started");
+});
